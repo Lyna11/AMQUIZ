25141f58a2cccf3f6f5211047413beddb73df42f
Félix Chabellard <felixchab@MacBook-Air-de-Felix.local>
2023-11-23 12:09:32 +0100

    logique quizz + timer a finaliser

--- src/screens/QuizzScreen.tsx
+++ src/screens/QuizzScreen.tsx
@@ -8,18 +8,41 @@ export default function QuizzScreen() {
 
     // Données questions.ts
     const data = questionsQuizz.questions;
+    const totalQuestions = data.length;
     // Index questions
     const [index, setIndex] = useState(0);
     const currentQuestion = data[index];
-    // Compteur points
-    const [points, setPoints] = useState(0);
-    // Statut réponse (True / False)
-    const [answerStatus, setAnswerStatus] = useState(false);
+    // Compteur score
+    const [score, setScore] = useState(0);
+    // Statut réponse (True = en cours / False = fini)
+    const [quizzStatus, setQuizzStatus] = useState(true);
     // Réponse choisie
     const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+    // Valeurs Timer
+    const [countdown, setCountdown] = useState<number>(10);
+    const [isPaused, setIsPaused] = useState<boolean>(true)
+
+    // Gestion du timer
+    useEffect(() => {
+        if (isPaused) return;
+        // Si le compteur atteint 0 on passe à la question suivante
+        if (countdown === 0) {
+            setIndex((index) => index + 1);
+            setCountdown(10);
+            setScore((score) => score - 5);
+        }
+        const interval = setInterval(() => {
+            setCountdown((countdown) => countdown - 1)
+        }, 1000);
+        // Reset
+        return () => clearInterval(interval);
+    }, [countdown, isPaused]);
 
     // Gestion choix réponses
     useEffect(() => {
+        // Lance le timer
+        setIsPaused(true);
+        // TODO : gestion du timer hors page / intermédiaire pour démarrer le quizz
         // Null check
         if (selectedAnswer === null || !currentQuestion) return;
         // Timeout
@@ -27,20 +50,23 @@ export default function QuizzScreen() {
         // Si la réponse est correcte...
         if (selectedAnswer === currentQuestion.answerId) {
             timeout = setTimeout(() => {
-                // TODO: surbrillance choix en vert
-                setPoints((points) => points + 10);
+                setScore((score) => score + 10);
                 setIndex((index) => index + 1);
-                setAnswerStatus(true);
+                setCountdown(10);
                 setSelectedAnswer(null);
                 console.log("bonne réponse");
             }, 1500);
         // Si la réponse est fausse...
         } else {
             timeout = setTimeout(() => {
-                // TODO: surbrillance choix en rouge
+                if (score > 5) {
+                    setScore((score) => score - 5);
+                } else {
+                    setScore(0);
+                }
                 setIndex((index) => index + 1);
-                setAnswerStatus(false);
-                setSelectedAnswer(null);
+                setCountdown(10);
+                setSelectedAnswer(null); 
                 console.log("mauvaise réponse");
             }, 1500);
         }
@@ -53,7 +79,7 @@ export default function QuizzScreen() {
     // Fin du Quizz (plus de questions)
     useEffect(() => {
         if (currentQuestion == null || data[index].question == null) {
-            console.log("FIN DU QUIZZ");
+            setQuizzStatus(false);
         }
     }, [currentQuestion]);
 
@@ -61,12 +87,25 @@ export default function QuizzScreen() {
     return (
         <SafeAreaView>
             <Text style={styles.title}>QUIZZ</Text>
-            <Text style={styles.question}>{currentQuestion?.question}</Text>
+
+            <Text style={styles.timer}>TIMER: {countdown}s</Text>
+
+            <Text style={styles.counter}>{quizzStatus === true ? "Score : " + score : 'Quizz terminé ! Résultat : ' + score + '/' + (totalQuestions * 10)}</Text>
+            
+            <Text style={styles.question}>
+                {index + 1 > data.length ? undefined : '(' + (index + 1) + '/' + totalQuestions + ')'} {currentQuestion?.question}
+            </Text>
+            
             <View style={{ marginTop: 10 }}>
                 {currentQuestion?.choices.map((item, index) => (
-                    <TouchableOpacity key={index} onPress={() => setSelectedAnswer(item.id)} style={{ borderColor: "red", padding: 10 }}>
+                    <TouchableOpacity key={index} onPress={() => setSelectedAnswer(item.id)} style={{ borderColor: "red", padding: 10}}>
+                        
                         <View style={styles.cards}> 
-                            <Text>{item.id}. {item.answer}</Text>
+
+                            <Text style={{color: selectedAnswer === item.id ? (selectedAnswer === currentQuestion.answerId ? 'green' : 'red') : undefined}}>
+                                {item.id}. {item.answer}
+                            </Text>
+                        
                         </View>
                     </TouchableOpacity>
                 ))}
@@ -79,29 +118,45 @@ export default function QuizzScreen() {
 
 /* Pour CSS */
 const styles = StyleSheet.create({
-  title: {
-    fontWeight: "bold",
-    fontSize: 30,
-    color: '#e91e63',
-    padding: 10
-  },
-  question: {
-    fontWeight: "bold",
-    fontSize: 16,
-    backgroundColor: '#e91e63',
-    padding: 5
-  },
-  cards: {
-    color: "black",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    borderWidth: 1,
-    borderColor: "#e91e63",
-    borderRadius: 20,
-    padding: 5,
-    maxWidth: 200,
-    textShadowRadius: 1,
-    textShadowColor: "black",
+    title: {
+        fontWeight: "bold",
+        fontSize: 30,
+        color: '#e91e63',
+        padding: 10
+    },
+    question: {
+        fontWeight: "bold",
+        fontSize: 18,
+        padding: 5,
+        margin: 0
+    },
+    cards: {
+        color: "black",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "black",
+        borderRadius: 20,
+        padding: 5,
+        maxWidth: 200,
+        textShadowRadius: 1,
+        textShadowColor: "black",
+    },
+    counter: {
+        color: "white",
+        backgroundColor: '#e91e63',
+        padding: 10,
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 10
+    },
+    timer: {
+        fontSize: 18,
+        color: '#6A5ACD',
+        fontWeight: 'bold',
+        padding: 5,
+        marginBottom: 10,
+        paddingLeft: 10
   }
 });
