40438ce68e86f56ac1328790dd7fa0fab213fbfa
Ugo SH <ugosh16@gmail.com>
2023-11-24 16:30:03 +0100

    ajout profilescreen + openingscreen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,19 +1,106 @@
-import * as React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { useState } from 'react';
+import { StyleSheet, View, Text, Image, TouchableOpacity, SafeAreaView, ScrollView, FlatList, Dimensions} from 'react-native';
+import ProfileImgSelector from '../components/ProfileImgSelector';
+import ProfileUserInfos from '../components/ProfileUserInfos';
+import ProfileUserEdit from '../components/ProfileUserEdit';
 
 export default function ProfilScreen() {
+
+  const [active, setActive] = useState(0);
+
   // Rendu
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>PROFILE</Text>
-    </View>
-  );
-}
+    
+    <SafeAreaView style={{ flex: 1}}>
+
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#DBE9EE' }}>
+        <View style={styles.mainContainer}>
+
+          <ProfileUserInfos/>
+
+          <View style={styles.userProfilePicSkills}>
+              <TouchableOpacity 
+              onPress={() => {setActive(0)}}
+              style={active === 0 ? styles.userProfilePicSkillsFirstChildActive : styles.userProfilePicSkillsFirstChild}>
+                  <Text>Images</Text>
+              </TouchableOpacity>
+              <TouchableOpacity 
+              onPress={() => {setActive(1)}}
+              style={active === 1 ? styles.userProfilePicSkillsLastChildActive : styles.userProfilePicSkillsLastChild}>
+                  <Text >Skills</Text>
+              </TouchableOpacity>
+          
+          </View>
+
+          {active === 0 && <ProfileImgSelector/>}
+          {active === 1 && <ProfileUserEdit/>}
+
+        </View>
+      </View>
+      </ScrollView>
+
+    </SafeAreaView>
+
+  )}
 
 /* Pour CSS */
 const styles = StyleSheet.create({
   /* TODO */
+  mainContainer: {
+    width: '80%',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+
+  userProfilePicSkills: {
+    flexDirection: 'row',
+    marginTop: 40,
+    height: 60,
+    width: '100%',
+    borderWidth: 2,
+    borderRadius: 15,
+    justifyContent: 'space-between',
+    padding: 3,
+},
+
+userProfilePicSkillsFirstChild: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    height: '100%',
+},
+
+userProfilePicSkillsFirstChildActive: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    height: '100%',
+
+    backgroundColor: '#9bc1d3',
+    borderRadius: 13,
+    borderWidth: 2,
+},
+
+userProfilePicSkillsLastChild: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    height: '100%',
+},
+
+userProfilePicSkillsLastChildActive: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    height: '100%',
+
+    backgroundColor: '#9bc1d3',
+    borderRadius: 13,
+    borderWidth: 2,
+},
 });
--- src/screens/UnboxingScreen.tsx
+++ src/screens/UnboxingScreen.tsx
@@ -1,21 +1,80 @@
-import * as React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from 'react';
+import { StyleSheet, View, Text, SafeAreaView, Image, FlatList, Dimensions } from 'react-native';
 
 export default function OuvertureScreen() {
+  const screenwidth:number = Dimensions.get('window').width;
+
+  interface USER_CHESTS {
+    id: number;
+    nom: string;
+    image: string;
+  }
+
+  const USER_CHESTS: USER_CHESTS[] = [
+    {
+      id: 1,
+      nom: 'Coffre narutesque',
+      image: 'https://wallpapers-clan.com/wp-content/uploads/2022/07/funny-cat-22.jpg',
+    },
+    {
+      id: 2,
+      nom: 'Coffre rare',
+      image: 'https://wallpapers-clan.com/wp-content/uploads/2022/07/funny-cat-22.jpg',
+    },
+    {
+      id: 3,
+      nom: 'Coffre légendaire',
+      image: 'https://wallpapers-clan.com/wp-content/uploads/2022/07/funny-cat-22.jpg',
+    },
+  ];
+
+  const renderItem = ({ item }: { item: USER_CHESTS }) => (
+    <View key={item.nom} style={styles.chestItemContainer}>
+      <Text>{item.nom}</Text>
+      <Image
+        source={{ uri: item.image }}
+        style={{
+          width: '100%', // Utilise '100%' pour la largeur
+          aspectRatio: 1 / 1,
+          resizeMode: 'cover',
+        }}
+      />
+    </View>
+  );
 
   // Rendu
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>UNBOXING</Text>
-    </View>
+    <SafeAreaView>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#DBE9EE' }}>
+        <View style={styles.mainContainer}>
+          <FlatList
+            data={USER_CHESTS}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.flatListContainer}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
-  
 }
 
 /* Pour CSS */
 const styles = StyleSheet.create({
-  /* TODO */
+  mainContainer: {
+    width: '80%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  flatListContainer: {
+    width: '100%', // Ajuste ici pour utiliser la largeur de l'écran
+  },
+  chestItemContainer: {
+    backgroundColor: 'red',
+    marginBottom: 10,
+    padding: 15, // Ajuste ici pour la marge/padding autour de l'image
+  },
 });
